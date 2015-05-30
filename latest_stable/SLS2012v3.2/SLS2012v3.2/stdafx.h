84efb4186d0769ec8c0afdb341ca16530d8c6917
Charalambos Poullis <charalambos@poullis.org>
2015-05-30 14:09:14 +0300

    Create stdafx.h

--- latest_stable/SLS2012v3.2/SLS2012v3.2/stdafx.h
+++ latest_stable/SLS2012v3.2/SLS2012v3.2/stdafx.h
@@ -1,3 +1,11 @@
+//------------------------------------------------------------------------------------------------------------
+//* Copyright © 2010-2015 Immersive and Creative Technologies Lab, Cyprus University of Technology           *
+//* Link: http://www.theICTlab.org                                                                           *
+//* Software developer(s): Kyriakos Herakleous                                                               *
+//* Researcher(s): Kyriakos Herakleous, Charalambos Poullis                                                  *
+//*                                                                                                          *
+//* License: Check the file License.md                                                                       *
+//------------------------------------------------------------------------------------------------------------
 // stdafx.h : include file for standard system include files,
 // or project specific include files that are used frequently, but
 // are changed infrequently
