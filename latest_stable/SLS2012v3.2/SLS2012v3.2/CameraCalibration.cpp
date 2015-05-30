b5f420fedbd60ccee8ea83815239105c3375d3f4
Charalambos Poullis <charalambos@poullis.org>
2015-05-30 13:57:20 +0300

    Update CameraCalibration.cpp

--- latest_stable/SLS2012v3.2/SLS2012v3.2/CameraCalibration.cpp
+++ latest_stable/SLS2012v3.2/SLS2012v3.2/CameraCalibration.cpp
@@ -1,11 +1,9 @@
 //------------------------------------------------------------------------------------------------------------
-//* Copyright © 2010-2013 Immersive and Creative Technologies Lab, Cyprus University of Technology           *
+//* Copyright Â© 2010-2013 Immersive and Creative Technologies Lab, Cyprus University of Technology           *
 //* Link: http://ict.cut.ac.cy                                                                               *
 //* Software developer(s): Kyriakos Herakleous                                                               *
 //* Researcher(s): Kyriakos Herakleous, Charalambos Poullis                                                  *
 //*                                                                                                          *
-//* This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.*
-//* Link: http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US                                        *
 //------------------------------------------------------------------------------------------------------------
 
 #include "StdAfx.h"
