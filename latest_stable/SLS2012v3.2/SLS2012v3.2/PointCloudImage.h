a675a768017f3203a426d875f3ae120c60bf1500
Charalambos Poullis <charalambos@poullis.org>
2015-05-30 14:04:08 +0300

    Update PointCloudImage.h

--- latest_stable/SLS2012v3.2/SLS2012v3.2/PointCloudImage.h
+++ latest_stable/SLS2012v3.2/SLS2012v3.2/PointCloudImage.h
@@ -1,11 +1,10 @@
 //------------------------------------------------------------------------------------------------------------
-//* Copyright © 2010-2013 Immersive and Creative Technologies Lab, Cyprus University of Technology           *
-//* Link: http://ict.cut.ac.cy                                                                               *
+//* Copyright Â© 2010-2015 Immersive and Creative Technologies Lab, Cyprus University of Technology           *
+//* Link: http://www.theICTlab.org                                                                           *
 //* Software developer(s): Kyriakos Herakleous                                                               *
 //* Researcher(s): Kyriakos Herakleous, Charalambos Poullis                                                  *
 //*                                                                                                          *
-//* This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.*
-//* Link: http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US                                        *
+//* License: Check the file License.md                                                                       *
 //------------------------------------------------------------------------------------------------------------
 
 #pragma once
